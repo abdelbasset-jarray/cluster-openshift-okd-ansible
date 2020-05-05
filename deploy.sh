5648532f768e0cb1f499524d6ea1e57a4f530b3b
Abdelbasset JARRAY <jarrayabdelbasset@gmail.com>
2020-05-05 01:01:11 +0100

    delete deploy.sh

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-
-ssh-keygen -b 2048 -t rsa -f /root/.ssh/id_rsa -q -N ""
-for host in monitoring.smti.com.tn paas-node-infra.smti.com.tn paas-node-app.smti.com.tn; \
-    do ssh-copy-id -i /root/.ssh/id_rsa.pub $host; \
-    done
-
-ansible-playbook /home/vagrant/openshift-ansible/playbooks/byo/config.yml
